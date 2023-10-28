959245bceb78a0d0112ce6f41603c3754cd95f28
Ezra Engels <ezengels@iu.edu>
2023-10-27 23:44:45 -0400

    progress

--- converter/src/main.rs
+++ converter/src/main.rs
@@ -1,11 +1,19 @@
-use std::io;
+use std::{error::Error, io, process};
 
-fn main() {
+fn csvreader() -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::Reader::from_reader(io::stdin());
 
     for result in rdr.records() {
-        let record = result.expect("a CSV record");
+        let record = result?;
 
         println!("{:?}", record);
+    }
+    Ok(())
+}
+
+fn main() {
+    if let Err(err) = csvreader() {
+        println!("error running csvreader: {}", err);
+        process::exit(1);
     }
 }
