48e924daa2c89c8e5b1f58bf723d265e5ad4effe
Ezra Engels <ezengels@iu.edu>
2023-10-28 22:35:07 -0400

    records being printed as strings

--- converter/src/main.rs
+++ converter/src/main.rs
@@ -1,19 +1,9 @@
-use std::{error::Error, io, process};
-
-fn csvreader() -> Result<(), Box<dyn Error>> {
-    let mut rdr = csv::Reader::from_reader(io::stdin());
-
-    for result in rdr.records() {
-        let record = result?;
-
-        println!("{:?}", record);
-    }
-    Ok(())
-}
+use std::io;
 
 fn main() {
-    if let Err(err) = csvreader() {
-        println!("error running csvreader: {}", err);
-        process::exit(1);
+    let mut reader = csv::Reader::from_reader(io::stdin());
+    for result in reader.records() {
+        let record = result.expect("a CSV record");
+        println!("{:?}", record);
     }
 }
